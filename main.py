bc1132e4eead2e8729541fecbcbf457d07b76776
pikitosdoritos <fastovetsn@gmail.com>
2026-03-19 16:50:05 +0200

    complete task "Searching first unique symbol"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#1 
+# 1 
 # We have a list of numbers. We need to find two numbers that in sum will be equal our target. Return indexes of these numbers. 
 
 nums = [10, 100, 22, 2, 9, 11, 10, 7, 22, 15]
@@ -15,3 +15,23 @@ def find_indx():
             return [seen[need], i]
         
 print(find_indx())
+
+# 2
+# Searching first unique symbol
+
+s = "leetcode"
+
+frequency = {}
+
+def find_unique_symbol():
+    for ch in s:
+        if ch not in frequency:
+            frequency[ch] = 1
+        else:
+            frequency[ch] += 1
+            
+    for key, value in frequency.items():
+        if value == 1:
+            return s.index(key)
+            
+print(find_unique_symbol())
